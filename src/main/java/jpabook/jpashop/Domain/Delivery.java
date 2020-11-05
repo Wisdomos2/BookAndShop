32d3fc4fa568430024c986784e2f10fd053a01ae
MOJUNSEO <mojunseo49@naver.com>
2020-11-05 14:47:21 +0900

    fix entity error

--- src/main/java/jpabook/jpashop/Domain/Delivery.java
+++ src/main/java/jpabook/jpashop/Domain/Delivery.java
@@ -1,7 +1,13 @@
 package jpabook.jpashop.Domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Entity
+@Getter
+@Setter
 public class Delivery {
 
     @Id @GeneratedValue
--- src/main/java/jpabook/jpashop/Domain/Order.java
+++ src/main/java/jpabook/jpashop/Domain/Order.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="orders")
@@ -46,4 +47,8 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+
+
+
 }
